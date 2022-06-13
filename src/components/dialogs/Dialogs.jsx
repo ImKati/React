ec62c71dacd6a5cf543ab49967cba11893d655b3
ImKati <kati.mihalevich@gmail.com>
2022-06-13 14:11:21 +0300

    message

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {findAllByDisplayValue} from "@testing-library/react";
+import * as PropTypes from "prop-types";
 
+function Navlink(props) {
+    return null;
+}
+
+Navlink.propTypes = {
+    to: PropTypes.string,
+    children: PropTypes.node
+};
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <Navlink to="/dialogs/1"> Dimych</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to="/dialogs/2">Andrey</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to="/dialogs/3">Sveta</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to="/dialogs/4">Sasha</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to="/dialogs/5">Victor</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to="/dialogs/6">Valera</Navlink>
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are you?</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
